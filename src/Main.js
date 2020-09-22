86806f4966627779841ca80a3fa410d107ed9148
Sasha Lukas <sashamati@gmail.com>
2020-09-22 09:06:08 -0700

    react-router-dom added, removed proj template and add Main.js, updated index.js and html

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Sasha Lukas Portfolio</h1>
+                <ul className="header">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/stuff">Project</a></li>
+                    <li><a href="/stuff">Resume</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+                <div className="content">
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
