c028b47fc30247586e5d81c4cf60b8e10e7689ea
Sasha Lukas <sashamati@gmail.com>
2020-09-22 09:39:36 -0700

    finished framing, included react router and has working nav links

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+import Home from "./pages/Home.js";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume"
 
 class Main extends Component {
     render() {
         return (
-            <div>
-                <h1>Sasha Lukas Portfolio</h1>
-                <ul className="header">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/stuff">Project</a></li>
-                    <li><a href="/stuff">Resume</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-                <div className="content">
-
+            <HashRouter>
+                <div>
+                    <h1>Sasha Lukas Portfolio</h1>
+                    <ul className="header">
+                        <li><NavLink to="/">Home</NavLink></li>
+                        <li><NavLink to="/project">Project</NavLink></li>
+                        <li><NavLink to="/contact">Contact</NavLink></li>
+                        <li><NavLink to="/resume">Resume</NavLink></li>
+                    </ul>
+                    <div className="content">
+                        <Route path="/" component={Home} />
+                        <Route path="/project" component={Project} />
+                        <Route path="/contact" component={Contact} />
+                        <Route path="/resume" component={Resume} />
+                    </div>
                 </div>
-            </div>
+            </HashRouter>
         );
     }
 }
