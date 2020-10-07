f75fea7db43ea0c1d5c0a984d8c4712d2645acba
Sasha Lukas <sashamati@gmail.com>
2020-10-07 12:46:29 -0700

    adding about me. made a skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Skills extends Component {
+    render() {
+        return (
+            <div className="skill-content">
+            <h2>Favorite Tools:</h2>
+            <ul>
+              <li>React.JS </li>
+              <li>Javascript </li>
+              <li>Python </li>
+              <li>Django </li>
+            </ul>
+          </div>
+        );
+    }
+}
+
+export default Skills 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Skills from "../components/Skills";
+
 // import React from 'react';
 
 
@@ -7,15 +9,16 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h2>HELLO</h2>
-                <p>Cras facilisis urna ornare ex volutpat, et
-                convallis erat elementum. Ut aliquam, ipsum vitae
-                gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-                metus nec massa. Maecenas hendrerit laoreet augue
-                nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
-
-                <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+                <h2>About Me:</h2>
+                <p>My name is Sasha Lukas and I'm a native born from South Missippi, Gulfport.
+                    I am a roaming transplant, that has spent years traveling the country working and exploring.
+                    In 2011, made my home in the western portion of the state of Washington. My prior work history was odd end
+                    jobs, bartending, and two years of store management. On many occassions I found myself coding, photography,
+                    and film to help boost visibility. 
+                </p>
+                <hr />
+                <br />
+                <p><Skills/></p>
             </div>
         );
     }
